9af8b8e57f445da8d66358a33a276fbda4b7aa15
pluruel <pluruel@gmail.com>
2019-12-02 23:51:49 +0900

    Impl import

--- src/containers/SVGImport.js
+++ src/containers/SVGImport.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-import { selectTool } from '../modules/svgcanvas';
+import { selectTool, imports } from '../modules/svgcanvas';
+import { parse, stringify } from 'svgson';
+const uploadFile = (event, imports) => {
+  let file = event.target.files[0];
 
-const SVGImport = ({ selectedTool, selectTool }) => <input type="file" />;
+  const fr = new FileReader();
+
+  fr.onload = () => {
+    parse(fr.result).then(e => imports(e.children));
+  };
+};
+
+const SVGImport = ({ selectedTool, selectTool, imports }) => (
+  <input type="file" onChange={e => uploadFile(e, imports)} />
+);
 
 let mapStateToProps = ({ svgcanvas }) => ({
   selectedTool: svgcanvas.selectedTool,
@@ -11,6 +23,7 @@ let mapStateToProps = ({ svgcanvas }) => ({
 
 const mapFunction = {
   selectTool,
+  imports,
 };
 
 export default connect(mapStateToProps, mapFunction)(SVGImport);
--- src/modules/svgcanvas.js
+++ src/modules/svgcanvas.js
@@ -7,6 +7,9 @@ const ADD_SHAPE = 'svgcanvas/ADD_SHAPE';
 const UNDO = 'svgcanvas/UNDO';
 const REDO = 'svgcanvas/REDO';
 const REMOVE = 'svgcanvas/REMOVE';
+const IMPORTS = 'svgcanvas/IMPORTS';
+const EXPORTS = 'svgcanvas/EXPORTS';
+const INITIALSTATE = 'svgcanvas/INITIALSTATE';
 
 export const selectColor = createAction(SELECT_COLOR);
 export const selectTool = createAction(SELECT_TOOL);
@@ -15,6 +18,9 @@ export const remove = createAction(REMOVE);
 export const addShape = createAction(ADD_SHAPE);
 export const undo = createAction(UNDO);
 export const redo = createAction(REDO);
+export const imports = createAction(IMPORTS);
+export const exports = createAction(EXPORTS);
+export const initialstate = createAction(INITIALSTATE);
 
 const initialState = {
   selectedColor: '#000000',
@@ -73,6 +79,16 @@ const svgcanvas = handleActions(
     [REDO]: (state, action) => ({
       ...state,
       currentStep: state.currentStep + 1,
+    }),
+    [INITIALSTATE]: (state, action) => ({
+      state: initialState,
+    }),
+    [IMPORTS]: (state, action) => ({
+      ...state,
+      obj: {
+        ...state.obj,
+        [state.currentStep]: action.payload,
+      },
     }),
   },
   initialState,
